d067f66ce3bb5504e95e46c708f2ab3314b593f4
MathieuC <oomtm453@gmail.com>
2026-04-27 20:16:18 -0400

    Adjusted to new Puck update (B312).

--- BetterGoalTriggers/Constants.cs
+++ BetterGoalTriggers/Constants.cs
@@ -3,7 +3,7 @@ namespace oomtm450PuckMod_BetterGoalTriggers {
         /// <summary>
         /// Const string, added current Puck Application.version to check for mod compatibility.
         /// </summary>
-        internal const string CURRENT_APPLICATION_VERSION = "202";
+        internal const string CURRENT_APPLICATION_VERSION = "312";
 
         /// <summary>
         /// Const string, prefix of all the mod's names.
